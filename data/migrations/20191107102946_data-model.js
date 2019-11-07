47100e54546ee2c30c799c37eb4a378fa958867b
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-11-07 10:54:53 -0800

    animal-zoos

--- data/migrations/20191107102946_data-model.js
+++ data/migrations/20191107102946_data-model.js
@@ -1,6 +1,10 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('animal-zoos', tbl => {
+      tbl.increments();
+      tbl.string('name', 255).notNullable();
+    })
 };
 
 exports.down = function(knex) {
